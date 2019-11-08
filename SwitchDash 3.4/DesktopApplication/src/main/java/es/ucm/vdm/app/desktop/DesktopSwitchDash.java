c12c608967a9b0836f3ff80480bd51f262a25633
Nestor <nestor.cabrero@ucm.es>
2019-11-08 12:04:21 +0100

    Changed Main class for DesktopSwitchDash

--- SwitchDash 3.4/DesktopApplication/src/main/java/es/ucm/vdm/app/desktop/DesktopSwitchDash.java
+++ SwitchDash 3.4/DesktopApplication/src/main/java/es/ucm/vdm/app/desktop/DesktopSwitchDash.java
@@ -6,6 +6,9 @@ import es.ucm.vdm.logic.DemoState;
 
 public class DesktopSwitchDash extends DesktopGame {
 
+    static final int windowWidth = 1280;
+    static final int windowHeight = 720;
+
     public DesktopSwitchDash(String windowName, int width, int height) {
         super(windowName, width, height);
     }
@@ -13,5 +16,9 @@ public class DesktopSwitchDash extends DesktopGame {
     @Override
     public State getStartState() {
         return new DemoState(this);
+    }
+
+    public static void main(String[] args) {
+        DesktopSwitchDash game = new DesktopSwitchDash("SwitchDash", windowWidth, windowHeight);
     }
 }
