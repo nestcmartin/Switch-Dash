cff9baa4298e90ba71615d14d1c80ea0776e4f76
Kelvin-CD <kelvinco@ucm.es>
2019-11-08 13:38:25 +0100

    DesktopMouseHandler now also implements MouseMotionListener

--- SwitchDash 3.4/DesktopEngine/src/main/java/es/ucm/vdm/engine/desktop/DesktopMouseHandler.java
+++ SwitchDash 3.4/DesktopEngine/src/main/java/es/ucm/vdm/engine/desktop/DesktopMouseHandler.java
@@ -2,6 +2,7 @@ package es.ucm.vdm.engine.desktop;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +10,7 @@ import javax.swing.JFrame;
 
 import es.ucm.vdm.engine.Input;
 
-public class DesktopMouseHandler implements MouseListener {
+public class DesktopMouseHandler implements MouseListener, MouseMotionListener {
 
     List<Input.TouchEvent> touchEventsBuffer_ = new ArrayList<Input.TouchEvent>();
 
@@ -26,38 +27,56 @@ public class DesktopMouseHandler implements MouseListener {
         return touchEventListCopy;
     }
 
+    // MouseListener
+
     @Override
-    public void mouseClicked(MouseEvent e) {
+    public void mouseClicked(MouseEvent mouseEvent) {
     }
 
     @Override
-    public void mouseEntered(MouseEvent e) {
+    public void mouseEntered(MouseEvent mouseEvent) {
     }
 
     @Override
-    public void mouseExited(MouseEvent e) {
+    public void mouseExited(MouseEvent mouseEvent) {
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
+    public void mousePressed(MouseEvent mouseEvent) {
         Input.TouchEvent tEvent = new Input.TouchEvent();
         tEvent.type_ = Input.EventType.PRESSED;
-        tEvent.x_ = e.getX();
-        tEvent.y_ = e.getY();
+        tEvent.x_ = mouseEvent.getX();
+        tEvent.y_ = mouseEvent.getY();
         synchronized(this) {
             touchEventsBuffer_.add(tEvent);
         }
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {
+    public void mouseReleased(MouseEvent mouseEvent) {
         Input.TouchEvent tEvent = new Input.TouchEvent();
         tEvent.type_ = Input.EventType.RELEASED;
-        tEvent.x_ = e.getX();
-        tEvent.y_ = e.getY();
+        tEvent.x_ = mouseEvent.getX();
+        tEvent.y_ = mouseEvent.getY();
         synchronized(this) {
             touchEventsBuffer_.add(tEvent);
         }
     }
 
+    // MouseMotionListener
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+        Input.TouchEvent tEvent = new Input.TouchEvent();
+        tEvent.type_ = Input.EventType.MOVED;
+        tEvent.x_ = mouseEvent.getX();
+        tEvent.y_ = mouseEvent.getY();
+        synchronized(this) {
+            touchEventsBuffer_.add(tEvent);
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+    }
 }
